d8c9c085cbb5da3ec80a9a33fcf48af7e61e5d39
nono <collegeus@users.noreply.github.com>
2014-03-26 00:28:04 -0400

    comments

--- icf.hs
+++ icf.hs
@@ -1,6 +1,15 @@
 import System.Environment   
 import Data.List 
 
+-- . every line read is trimmed
+-- . lines starting with #, //, or -- are comments
+-- . first column specifies selector, second denotes group, the rest defines attributes
+-- . selectos form tree structure
+-- . groups are sets of symbols; groups can be spcifies as per set operations; symbols separated by , can appear as group
+-- . #include in effect copies the content of included file in place, recursive include incurs program exit
+-- . diff displayed per symbol, or per group if possible
+-- . xuefei yang
+
 main = do
     args <- getArgs
     progName <- getProgName
